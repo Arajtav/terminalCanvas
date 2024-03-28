2cd2b5ef02b08d3d413a81e4304384e57cd4ac03
Arajtav <arajtav@gmail.com>
2024-03-28 23:00:44 +0100

    mostly working interpolated triangles

--- src/cavasExtra2d.go
+++ src/cavasExtra2d.go
@@ -2,6 +2,7 @@ package terminalCanvas
 
 import (
     "math"
+    "sort"
 )
 
 func interpolateColor(a Color, b Color, t float64) Color {
@@ -50,4 +51,77 @@ func (c *Canvas) DrawLineI(a U16Vec2C, b U16Vec2C) {
             cp.Y += g.Y;
         }
     }
+}
+
+func (c *Canvas) DrawLineIC(a I16Vec2C, b I16Vec2C) {
+    na := cvPosCenter(I16Vec2{a.X, a.Y}, c.sizeX, c.sizeY);
+    nb := cvPosCenter(I16Vec2{b.X, b.Y}, c.sizeX, c.sizeY);
+    c.DrawLineI(U16Vec2C{na.X, na.Y, a.C}, U16Vec2C{nb.X, nb.Y, b.C});
+}
+
+func getLineI(a I16Vec2C, b I16Vec2C) []I16Vec2C {
+    d := I16Vec2{b.X - a.X, b.Y - a.Y};
+    g := I16Vec2{1, 1};
+
+    if d.X < 0 {
+        d.X = -d.X;
+        g.X = -1;
+    }
+    if d.Y < 0 {
+        d.Y = -d.Y;
+        g.Y = -1;
+    }
+
+    err := d.X - d.Y;
+    cp := I16Vec2{a.X, a.Y};
+
+    var points []I16Vec2C;
+
+    for {
+        points = append(points, I16Vec2C{cp.X, cp.Y, interpolateColor(a.C, b.C, distI16Vec2(I16Vec2{a.X, b.X}, cp)/distI16Vec2(I16Vec2{a.X, a.Y}, I16Vec2{b.X, b.Y}))});
+        if cp.X == b.X && cp.Y == b.Y { break; }
+
+        e2 := 2 * err;
+        if e2 > -d.Y {
+            err -= d.Y;
+            cp.X += g.X;
+        }
+        if e2 < d.X {
+            err += d.X;
+            cp.Y += g.Y;
+        }
+    }
+    return points;
+}
+
+// TODO THIS IS BROKEN
+func (c *Canvas) DrawTriangleI(p0 U16Vec2C, p1 U16Vec2C, p2 U16Vec2C) {
+    var points []I16Vec2C;
+    points = append(points, getLineI(I16Vec2C{int16(p0.X), int16(p0.Y), p0.C}, I16Vec2C{int16(p1.X), int16(p1.Y), p1.C})...);
+    points = append(points, getLineI(I16Vec2C{int16(p1.X), int16(p1.Y), p1.C}, I16Vec2C{int16(p2.X), int16(p2.Y), p2.C})...);
+    points = append(points, getLineI(I16Vec2C{int16(p2.X), int16(p2.Y), p2.C}, I16Vec2C{int16(p0.X), int16(p0.Y), p0.C})...);
+
+    sort.Slice(points, func(i int, j int) bool {
+        return points[i].Y < points[j].Y || (points[i].Y == points[j].Y && points[i].X <= points[j].X);
+    });
+
+    i := 0;
+    for i<len(points) {
+        startp := points[i];
+        j := i;
+        for points[j].Y == points[i].Y {    // finding last position with same y
+            j++;
+            if j >= len(points) { break; }
+        }
+        endp := points[j-1];
+        c.DrawLineI(U16Vec2C{uint16(startp.X), uint16(startp.Y), startp.C}, U16Vec2C{uint16(endp.X), uint16(endp.Y), endp.C});
+        i = j;
+    }
+}
+
+func (c *Canvas) DrawTriangleIC(p0 I16Vec2C, p1 I16Vec2C, p2 I16Vec2C) {
+    p0n := cvPosCenter(I16Vec2{p0.X, p0.Y}, c.sizeX, c.sizeY);
+    p1n := cvPosCenter(I16Vec2{p1.X, p1.Y}, c.sizeX, c.sizeY);
+    p2n := cvPosCenter(I16Vec2{p2.X, p2.Y}, c.sizeX, c.sizeY);
+    c.DrawTriangleI(U16Vec2C{p0n.X, p0n.Y, p0.C}, U16Vec2C{p1n.X, p1n.Y, p1.C}, U16Vec2C{p2n.X, p2n.Y, p2.C});
 }
